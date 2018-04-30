03eb54e931933d8739e1f4cd13d8265f9023fd2e
Quan Chu <quanchu296@gmail.com>
2018-04-29 22:29:47 -0700

    182

--- TestMakerFreeWebApp/ClientApp/app/app.shared.module.ts
+++ TestMakerFreeWebApp/ClientApp/app/app.shared.module.ts
@@ -8,13 +8,21 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuizListComponent } from './components/quiz/quiz-list.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         HomeComponent,
-        QuizListComponent
+        QuizListComponent,
+        QuizComponent,
+        AboutComponent,
+        LoginComponent,
+        PageNotFoundComponent
     ],
     imports: [
         CommonModule,
@@ -23,7 +31,10 @@ import { QuizListComponent } from './components/quiz/quiz-list.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: 'quiz/:id', component: QuizComponent },
+            { path: 'about', component: AboutComponent },
+            { path: 'login', component: LoginComponent },
+            { path: '**', component: PageNotFoundComponent }
         ])
     ]
 })
